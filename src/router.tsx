f757c69858664d0497d757efbd3b23e8f3aedcde
monster-wu <464711201@qq.com>
2023-09-04 23:14:29 +0800

    feat: 活动管理平台-layout调整

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 
 import { RouterAPI } from "dva";
-import { Router, Route } from "dva/router";
+import { Router, Route } from "dva/router"; // dva中的router和react-router-dom一样
 import Layout from "./layout/layout";
 
 export default (api?: RouterAPI) => {
